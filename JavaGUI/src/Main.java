5f9893648eb612a58e3b3379215ede86f4a2d4e9
kyle <kylegwapo04@gmail.com>
2021-08-22 23:51:02 +0800

    'javawerks'

---
+++ JavaGUI/src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+
+//GUI frame = new GUI(500,400);
+//Label frame = new Label();
+//Panel panel = new Panel();
+
+		Button button = new Button();
+
+
+	}
+
+}
